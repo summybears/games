49b5f48af69caf58f55c2eeae05a4f66daf0967f
summybears <44300376+summybears@users.noreply.github.com>
2019-01-04 11:20:13 -0800

    1/4

--- Game/behavior-hx/ActorEvents_15.hx
+++ Game/behavior-hx/ActorEvents_15.hx
@@ -40,7 +40,6 @@ import box2D.common.math.B2Vec2;
 import box2D.dynamics.B2Body;
 import box2D.dynamics.B2Fixture;
 import box2D.dynamics.joints.B2Joint;
-import box2D.collision.shapes.B2Shape;
 
 import motion.Actuate;
 import motion.easing.Back;
@@ -70,18 +69,29 @@ import com.stencyl.graphics.shaders.BloomShader;
 
 
 
-class SceneEvents_1 extends SceneScript
+class ActorEvents_15 extends ActorScript
 {
 	
 	
-	public function new(dummy:Int, dummy2:Engine)
+	public function new(dummy:Int, actor:Actor, dummy2:Engine)
 	{
-		super();
+		super(actor);
 		
 	}
 	
 	override public function init()
 	{
+		
+		/* ======================== Actor of Type ========================= */
+		addCollisionListener(actor, function(event:Collision, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled && sameAsAny(getActorType(4), event.otherActor.getType(),event.otherActor.getGroup()))
+			{
+				recycleActor(actor);
+				playSound(getSound(47));
+				Engine.engine.setGameAttribute("Score", (Engine.engine.getGameAttribute("Score") + 10));
+			}
+		});
 		
 	}
 	
