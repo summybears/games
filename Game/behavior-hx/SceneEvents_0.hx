b645fd3f406185639b030ef5d18e477ab43f35f1
summybears <44300376+summybears@users.noreply.github.com>
2019-01-15 13:37:07 -0800

    1/15

--- Game/behavior-hx/SceneEvents_0.hx
+++ Game/behavior-hx/SceneEvents_0.hx
@@ -40,6 +40,7 @@ import box2D.common.math.B2Vec2;
 import box2D.dynamics.B2Body;
 import box2D.dynamics.B2Fixture;
 import box2D.dynamics.joints.B2Joint;
+import box2D.collision.shapes.B2Shape;
 
 import motion.Actuate;
 import motion.easing.Back;
@@ -69,26 +70,27 @@ import com.stencyl.graphics.shaders.BloomShader;
 
 
 
-class ActorEvents_0 extends ActorScript
+class SceneEvents_0 extends SceneScript
 {
 	
 	
-	public function new(dummy:Int, actor:Actor, dummy2:Engine)
+	public function new(dummy:Int, dummy2:Engine)
 	{
-		super(actor);
+		super();
 		
 	}
 	
 	override public function init()
 	{
 		
-		/* ======================== When Updating ========================= */
-		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
+		/* ========================= When Drawing ========================= */
+		addWhenDrawingListener(null, function(g:G, x:Float, y:Float, list:Array<Dynamic>):Void
 		{
 			if(wrapper.enabled)
 			{
-				Engine.engine.setGameAttribute("Hero X", actor.getX());
-				Engine.engine.setGameAttribute("Hero Y", actor.getY());
+				g.setFont(getFont(17));
+				g.drawString("" + "Score:", 13, 27);
+				g.drawString("" + Engine.engine.getGameAttribute("Score"), 90, 27);
 			}
 		});
 		
